d1bff8dfe7faa73dc100417d1920d30b212bf247
Yuri Adams <yuriadams@gmail.com>
2015-05-19 20:37:30 -0300

    retirando debugger e refazendo widget de calendario

--- jobs/aws.rb
+++ jobs/aws.rb
@@ -9,15 +9,10 @@ SCHEDULER.every '5s', :first_in => 0 do |job|
     instance = {name: 'bookmaker-production-nova', instance_id: "i-2ad391d0", region: 'us-east-1'}
     
     send_event "leitor10_cpu", { points: get_cpu_series(dashing_ec2, instance) }
-    send_event "leitor10_status_check", { current: get_status_check_failed(dashing_ec2, instance) }
 #    send_event "ec2-cpu-server3", { points: cpu_series[2][:data] }
 
 end
 
 def get_cpu_series dashing_ec2, instance
     dashing_ec2.getInstanceStats(instance[:instance_id], instance[:region], "CPUUtilization", :average)[:data]
-end 
-
-def get_status_check_failed dashing_ec2, instance
-    dashing_ec2.getInstanceStats(instance[:instance_id], instance[:region], "StatusCheckFailed", :average)[:data].last[:y].to_i
 end
--- jobs/calendar.rb
+++ jobs/calendar.rb
@@ -4,46 +4,20 @@ require 'active_support'
 require 'active_support/all'
 require 'debugger'
 
-# Update these to match your own apps credentials
-service_account_email = ENV["GOOGLE_API_CLIENT"] # Email of service account
-key_file = '#{Dir.pwd}/Dashing-Nel-Calendar-8a73b823fce7.p12' # File containing your private key
-key_secret = ENV["GOOGLE_API_KEY"] # Password to unlock private key
-calendarID = ENV["GOOGLE_CALENDAR_ID"] # Calendar ID.
-
-# Get the Google API client
-client = Google::APIClient.new(:application_name => 'Dashing-Nel-Calendar', :application_version => '0.0.1')
-
-# Load your credentials for the service account
-if not key_file.nil? and File.exists? key_file
-  key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
-end
-
-client.authorization = Signet::OAuth2::Client.new(
-  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-  :audience => 'https://accounts.google.com/o/oauth2/token',
-  :scope => 'https://www.googleapis.com/auth/calendar.readonly',
-  :issuer => service_account_email,
-  :signing_key => key)
-
 # Start the scheduler
 SCHEDULER.every '5s', :first_in => 4 do |job|
+#result.data
+  send_event('calendar', { events: get_events })
+end
 
-  # Request a token for our service account
-  client.authorization.fetch_access_token!
-
-  # Get the calendar API
-  service = client.discovered_api('calendar','v3')
-
-  # Start and end dates
-  now = DateTime.now
-
-  result = client.execute(:api_method => service.events.list,
-                          :parameters => {'calendarId' => calendarID,
-                                          'timeMin' => now.rfc3339,
-                                          'orderBy' => 'startTime',
-                                          'singleEvents' => 'true',
-                                          'maxResults' => 6})  # How many calendar items to get
-
-  send_event('calendar', { events: result.data })
-
+def get_events
+  uri = URI("https://www.googleapis.com/calendar/v3/calendars/#{ENV["GOOGLE_CALENDAR_ID"]}/events") 
+  
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  headers = { "Authorization" => "Bearer #{ENV["GOOGLE_API_KEY"]}" }
+  
+  response = http.request(Net::HTTP::Get.new(uri.request_uri, headers))
+  
+  JSON.parse(response.body)["items"] #.map{|item| { label: item["level"], occurrences: item["unique_occurrences"] }}
 end
