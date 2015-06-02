d5af81da4b0f271537805dfd981e28854dfb83ed
Fernando Martínez <fernando.xps@gmail.com>
2015-06-02 14:47:13 -0300

    [ #1] Finish tv layout

--- jobs/rollbar_errors.rb
+++ jobs/rollbar_errors.rb
@@ -3,19 +3,18 @@ require 'pry'
 
 SCHEDULER.every '60s' do
   atlas = rollbar_errors ENV["ROLLBAR_ACCESS_TOKEN_ATLAS"]
-  # leitor10 = rollbar_errors ENV["ROLLBAR_ACCESS_TOKEN_LEITOR10"]
-
-  send_event('atlas_rollbar_errors', { value: atlas } )
-  # send_event('leitor10_rollbar_errors', { value: leitor10 } )
+  leitor10 = rollbar_errors ENV["ROLLBAR_ACCESS_TOKEN_LEITOR10"]
+  send_event('atlas_rollbar_errors', { items: atlas } )
+  send_event('leitor10_rollbar_errors', { items: leitor10 } )
 end
 
 def rollbar_errors access_token
   response = JSON.parse(RestClient.get "https://api.rollbar.com/api/1/items/", params: { access_token: "#{access_token}", status: "active" })
   items = response['result']['items'].map { |item| { label: item["level"], occurrences: item["unique_occurrences"] } }
   [
-    get_count_by(items, 'error'),
-    get_count_by(items, 'warning'),
-    get_count_by(items, 'info')
+    get_count_by(items, 'Errors'),
+    get_count_by(items, 'Warnings'),
+    get_count_by(items, 'Infos')
   ]
 end
 
