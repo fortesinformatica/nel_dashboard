6049aa67d095833f64f9c3ea215a095e0386fdd0
Yuri Adams <yuriadams@gmail.com>
2015-06-25 14:17:51 -0300

    separando requests do code climate

--- jobs/codeclimate_leitor10.rb
+++ jobs/codeclimate_leitor10.rb
@@ -1,14 +1,11 @@
 require 'rest-client'
 require 'pry'
 
-SCHEDULER.every '10m', :first_in => 0 do |job|
-  atlas_stats = project_status ENV["CODE_CLIMATE_ATLAS_ID"]
-  # leitor10_stats = project_status ENV["CODE_CLIMATE_LEITOR10_ID"]
-  send_event("atlas_codeclimate", atlas_stats)
-  send_event("atlas_coverage", atlas_stats)
+SCHEDULER.every '10m', :first_in => 60 do |job|
+  leitor10_stats = project_status ENV["CODE_CLIMATE_LEITOR10_ID"]
 
-  # send_event("leitor10_codeclimate", leitor10_stats)
-  # send_event("leitor10_coverage", leitor10_stats)
+  send_event("leitor10_codeclimate", leitor10_stats)
+  send_event("leitor10_coverage", leitor10_stats)
 end
 
 def project_status repo_id
