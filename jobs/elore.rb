30bbc46757f973fd4e1925dd89aba5ef68e82880
Pedro Benevides <pedro.augusto.sb@gmail.com>
2016-06-21 10:05:28 -0300

    mudando intervalo de consulta dos usuarios do elore para 1h

--- jobs/elore.rb
+++ jobs/elore.rb
@@ -1,4 +1,4 @@
-SCHEDULER.every '10s', :first_in => 0 do |job|
+SCHEDULER.every '1h', :first_in => 0 do |job|
 
   url = ENV['ELORE_HOST'] || "http://localhost:3000/dashboard/users_counters.json"
   response = []
